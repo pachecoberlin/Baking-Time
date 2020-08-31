f45e5847dd99547fd7bbb32a7908e540fd16adf8
Alexander Wyron Wachtberger <awachtberger@assystemtechnologies.com>
2020-08-31 09:05:29 +0200

    inital commit main and master-detail-flow

---
+++ app/src/main/java/de/pacheco/bakingapp/RecipeListActivity.java
@@ -0,0 +1,39 @@
+package de.pacheco.bakingapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, ItemListActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
+        View recyclerView = findViewById(R.id.recipe_list);
+        assert recyclerView != null;
+//        setupRecyclerView((RecyclerView) recyclerView);
+    }
+}
