090a2da5cd90469622d3aa073223b42eb3a8c2e3
pacheco <pacheco@gmx.de>
2020-09-04 11:35:35 +0200

    Image or Video or nothing and text is hoswn in recipe step

--- app/src/main/java/de/pacheco/bakingapp/activities/StepDetailFragment.java
+++ app/src/main/java/de/pacheco/bakingapp/activities/StepDetailFragment.java
@@ -5,16 +5,20 @@ import de.pacheco.bakingapp.model.Recipe;
 import de.pacheco.bakingapp.model.Steps;
 import de.pacheco.bakingapp.utils.Utils;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
@@ -22,6 +26,10 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -36,13 +44,17 @@ public class StepDetailFragment extends Fragment {
      */
     public static final String STEPS_ID = "stepsID";
     public static final String RECIPE_ID = "recipeID";
+    public static final String IMAGE = "image";
+    public static final String VIDEO = "video";
+    public static final String TAG = StepDetailFragment.class.getSimpleName();
 
     /**
      * The Recipe and Step number this fragment is presenting.
      */
     private Recipe recipe;
     private Steps step;
-    private Activity activity;
+    private FragmentActivity activity;
+    private PlayerView playerView;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -81,8 +93,6 @@ public class StepDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.step_detail, container, false);
-        // TODO depending if there is a video thumbnail url or neither the view must be
-        //  different, maybe Framelayout. thumbnail shall be an image, but is in example a video...
         if (recipe != null) {
             TextView textView = (TextView) rootView.findViewById(R.id.item_detail);
             textView.setText(step.description);
@@ -91,35 +101,88 @@ public class StepDetailFragment extends Fragment {
         return rootView;
     }
 
-//TODO test with image   String testimg = "https://upload.wikimedia" +
-//            ".org/wikipedia/commons/thumb/f/fc/Banknote_portrait_pattern_%28Intaglio_print%2C_tactile_effect%29.jpg/400px-Banknote_portrait_pattern_%28Intaglio_print%2C_tactile_effect%29.jpg";
-
     private void setupPlayer(View rootView) {
-        //TODO full video screen on rotating to landscape
-        PlayerView playerView = (PlayerView) rootView.findViewById(R.id.playerView);
-        String videoURL = step.videoURL == null || step.videoURL.isEmpty() ? step.thumbnailURL : step.videoURL;
-        if (videoURL == null || videoURL.isEmpty()) {
+        //TODO full video screen on rotating to landscape as well as zeitpunkt vom play
+        // weiterführen und nicht neu anfangen und so
+        playerView = rootView.findViewById(R.id.playerView);
+        NestedScrollView textView = rootView.findViewById(R.id.item_detail_scrollview);
+        ImageView imageView = rootView.findViewById(R.id.step_image);
+        String urlString = step.videoURL == null || step.videoURL.isEmpty() ? step.thumbnailURL : step.videoURL;
+        if (urlString == null || urlString.isEmpty()) {
+            switchToOnlyText(playerView, textView, imageView);
+            return;
+        }
+        Thread thread = new Thread(setPlayerOrImageContent(playerView, textView, imageView,
+                urlString));
+        thread.start();
+    }
+
+    private void switchToOnlyText(PlayerView playerView, NestedScrollView textView, ImageView imageView) {
+        if (playerView != null) {
             playerView.setVisibility(View.INVISIBLE);
-            //TODO entweder framelayout oder hier noch die TextView groß machen
-            return;
         }
-        playerView.setUseArtwork(true);
-//        Drawable drawable; //TODO check ob in der URL ein Bild oder Video ist und wenn bild dann
-        // ein Drawable daraus machen
-//        playerView.setDefaultArtwork(drawable);
-
-
-        if (activity == null) {
-            return;
+        if (imageView != null) {
+            imageView.setVisibility(View.INVISIBLE);
         }
-        SimpleExoPlayer player = new SimpleExoPlayer.Builder(activity).build();
-        playerView.setPlayer(player);
-        Uri uri = Uri.parse(videoURL);
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(activity,
-                "bakingStep");
-        MediaSource mediaSource =
-                new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
-        player.setPlayWhenReady(true);
-        player.prepare(mediaSource, false, false);
+        if (textView != null && textView.getLayoutParams() != null) {
+            textView.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+        }
+    }
+
+    private void switchToImageOrVideo(PlayerView playerView, ImageView imageView, boolean isVideo) {
+        if (playerView != null) {
+            playerView.setVisibility(isVideo ? View.VISIBLE : View.INVISIBLE);
+        }
+        if (imageView != null) {
+            imageView.setVisibility(isVideo ? View.INVISIBLE : View.VISIBLE);
+        }
+    }
+
+    private Runnable setPlayerOrImageContent(PlayerView playerView, NestedScrollView textView, ImageView imageView, String urlString) {
+        return () -> {
+            try {
+                URL url = new URL(urlString);
+                URLConnection urlConnection = url.openConnection();
+                String contentType = urlConnection == null ? "" : urlConnection.getContentType();
+                if (contentType.startsWith(IMAGE)) {
+                    if (imageView == null) {
+                        return;
+                    }
+                    imageView.post(() -> {
+                        switchToImageOrVideo(playerView, imageView, false);
+                        Picasso.get().load(urlString).error(R.drawable.ic_food).into(imageView);
+                    });
+                } else if (contentType.startsWith(VIDEO)) {
+                    if (playerView == null) {
+                        return;
+                    }
+                    if (activity == null) {
+                        return;
+                    }
+                    SimpleExoPlayer player = new SimpleExoPlayer.Builder(activity).build();
+                    Uri uri = Uri.parse(urlString);
+                    DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                            activity, "bakingStep");
+                    MediaSource mediaSource = new ProgressiveMediaSource.Factory(
+                            dataSourceFactory).createMediaSource(uri);
+                    playerView.post(() -> {
+                        playerView.setPlayer(player);
+                        player.setPlayWhenReady(true);
+                        player.prepare(mediaSource, false, false);
+                        switchToImageOrVideo(playerView, imageView, true);
+                    });
+                } else {
+                    switchToOnlyText(playerView, textView, imageView);
+                }
+            } catch (Throwable e) {
+                Log.e(TAG, "Error while checking URL", e);
+                if (textView != null) {
+                    textView.post(() -> {
+                        switchToOnlyText(playerView, textView, imageView);
+                        Toast.makeText(activity, "ERROR", Toast.LENGTH_LONG).show();
+                    });
+                }
+            }
+        };
     }
 }
